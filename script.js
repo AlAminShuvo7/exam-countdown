d8631f8faf55584ccae0de40544020c9e7a551f0
Al Amin Shuvo <67927325+AlAminShuvo7@users.noreply.github.com>
2025-08-11 16:05:57 +0600

    fix 'Expired' condition

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function go(timeStamp, n) {
   // Get today's date and time
   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
+  // Find the distance between now and the countdown date
   var distance = timeStamp - now;
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for days, hours, minutes, and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -60,7 +60,8 @@ function go(timeStamp, n) {
   document.getElementById(n).innerHTML = days + "Day(s) " + hours + ":"
   + minutes + ":" + seconds;
     
-  // If the count down is over, write some text 
+  // If the countdown is over, write some text 
+  var x = n.slice(0, n.length-3);
   if (distance < 0) {
     clearInterval(x);
       document.getElementById(n).innerHTML = "EXPIRED";
